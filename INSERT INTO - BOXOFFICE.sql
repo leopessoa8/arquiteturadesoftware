86d08fc4248c27bcbd6dd99a17bec30a030156ee
Leonardo Pessoa <87311548+leopessoa8@users.noreply.github.com>
2021-07-12 09:28:52 -0300

    Add files via upload

---
+++ INSERT INTO - BOXOFFICE.sql
@@ -0,0 +1,4 @@
+USE pixar;
+
+INSERT INTO boxoffice (movie_id, rating, domestic_sales, international_sales)
+VALUES (8, 6.8, 450000000, 370000000);
---
+++ rataatui.sql
@@ -0,0 +1,3 @@
+UPDATE pixar.movies
+SET title = 'Ratatoulle', year = 2007
+WHERE title = 0 AND year = 2010;
